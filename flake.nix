c4c9463bf1e2b7452ec3f9fb2bd3257c99e3e446
Diego Roccia <diego.roccia@zalando.de>
2024-10-27 19:57:54 +0100

    working now
    Signed-off-by: Diego Roccia <diego.roccia@zalando.de>

--- flake.nix
+++ flake.nix
@@ -1,33 +1,30 @@
 {
-  description = "Home Manager configuration of diegoroccia";
+  description = "Home Manager Configuration";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixGL = {
-      url = "github:nix-community/nixGL";
+      url = "github:nix-community/nixgl";
+    };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    wezterm = {
-      url = "github:wez/wezterm/main?dir=nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # wezterm = {
+    #   url = "github:wez/wezterm?dir=nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nixGL,
-    ...
-  } @ inputs: let
+  outputs = { self, nixpkgs, home-manager, nixGL, ... }@inputs: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
-      inherit system;
+      system = "x86_64-linux";
+      overlays = [ nixGL.overlay ];
       config = {
         allowUnfree = true;
         allowUnfreePredicate = _: true;
@@ -37,7 +34,7 @@
     homeConfigurations."diegoroccia" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
       extraSpecialArgs = {
-        inherit self nixpkgs inputs;
+        inherit self system nixpkgs inputs nixGL;
       };
       modules = [./home.nix];
     };
--- home.nix
+++ home.nix
@@ -1,49 +1,92 @@
-{
-  config,
-  pkgs,
-  lib,
-  inputs,
-  ...
-}: let
-  nixGLIntel = inputs.nixGL.packages.${pkgs.system}.nixGLIntel;
-in {
+{ inputs, pkgs, config, nixgl, ... }: {
 
   home = {
     username = "diegoroccia";
     homeDirectory = "/home/diegoroccia";
-    stateVersion = "24.05";
+    preferXdgDirectories = true;
+    stateVersion = "24.11";
     packages = with pkgs; [
-      hyprland
-      wezterm
-      # (config.lib.nixGL.wrap rofi)
-      # (config.lib.nixGL.wrap rofi-pass)
-      rofi-wayland
-      rofi-vpn
-      rofi-emoji
-      rofi-rbw-wayland
-      pinentry-rofi
-      google-chrome
-      dunst
-      neovim
-      wlogout
-      hyprlock
-      chezmoi
-      obsidian
-      cargo
-      easyeffects
-    ];
+	  hyprland
+	  wezterm
+	  rofi-wayland
+	  dunst
+	  wlogout
+	  hyprlock
+	  chezmoi
+	];
   };
-
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = _: true;
-    };
-  };
-
-  fonts.fontconfig.enable = true;
 
   programs.home-manager.enable = true;
+
+#   programs.wezterm = {
+#       enable = true;
+#       package = inputs.wezterm.packages.${pkgs.system}.default;
+#       enableZshIntegration = true;
+#       extraConfig = ''
+# local wezterm = require("wezterm")
+# local config = wezterm.config_builder() or {}
+#
+# config.enable_wayland = true
+# config.automatically_reload_config = true
+# config.front_end = "WebGpu"
+#
+# -- don't care about tabs
+# config.enable_tab_bar = true
+# config.use_fancy_tab_bar = true
+# config.show_tabs_in_tab_bar = true
+# config.show_new_tab_button_in_tab_bar = true
+#
+# -- colorscheme
+# -- config.color_scheme = "Catppuccin Mocha"
+# config.color_scheme = "Catppuccin Macchiato"
+#
+# config.font = wezterm.font_with_fallback({
+# 	{
+# 		family = "JetBrains Mono",
+# 		weight = "Regular",
+# 		harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+# 	},
+# 	{ family = "Noto Color Emoji", weight = "Regular" },
+# 	"Symbols Nerd Font Mono",
+# 	"Powerline Extra Symbols",
+# 	"codicon",
+# 	"Noto Sans Symbols",
+# 	"Noto Sans Symbols2",
+# 	"Font Awesome 6 Free",
+# })
+#
+# config.line_height = 1.5
+# config.underline_position = "-5px"
+# -- config.underline_thickness = "100%"
+# config.command_palette_font_size = 12
+#
+# config.window_padding = {
+# 	left = 20,
+# 	right = 20,
+# 	top = 20,
+# 	bottom = 20,
+# }
+#
+# config.keys = require("keys")
+#
+# -- config.default_prog = { "tmux" }
+#
+# config.tiling_desktop_environments = {
+# 	"X11 LG3D",
+# 	"X11 bspwm",
+# 	"X11 i3",
+# 	"X11 dwm",
+# 	"Wayland",
+# }
+#
+# config.adjust_window_size_when_changing_font_size = false
+#
+# return config
+#
+#     '';
+#   };
+
+  fonts.fontconfig.enable = true;
 
   programs.go = {
     enable = true;
@@ -51,7 +94,13 @@ in {
 
   xdg.configFile."environment.d/envvars.conf".text = ''
     PATH="$HOME/.nix-profile/bin:$PATH"
-    export XDG_DATA_DIRS="/home/diegoroccia/.nix-profile/share:$XDG_DATA_DIRS"
+    XDG_DATA_DIRS="/home/diegoroccia/.nix-profile/share:$XDG_DATA_DIRS"
   '';
+
+  # services.gpg-agent = {
+  #   enable = true;
+  #   defaultCacheTtl = 1800;
+  #   enableSshSupport = true;
+  # };
 
 }
